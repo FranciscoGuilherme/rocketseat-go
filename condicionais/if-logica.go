bca4caa5c358791fcd6c2b95e97ea54bbd6b1fda
Francisco Antunes <francisco.antunes@pravaler.com.br>
2025-06-25 14:16:31 -0300

    feat(project): :sparkles: implementa recursos referentes as estruturas condicionais.

---
+++ condicionais/if-logica.go
@@ -0,0 +1,12 @@
+package condicionais
+
+import (
+	"fmt"
+	"math"
+)
+
+func CondicionalLogica() {
+	if x := math.Sqrt(4); x < 10 {
+		fmt.Println("A raiz quadrada de 4 é menor que 10")
+	}
+}
---
+++ condicionais/if.go
@@ -0,0 +1,9 @@
+package condicionais
+
+import "fmt"
+
+func Condicional() {
+	if 1 < 2 {
+		fmt.Println("1 é menor que 2")
+	}
+}
