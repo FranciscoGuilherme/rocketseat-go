815db213380e870a4e55d950487fb0bfa589b6d9
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2025-07-13 20:15:20 -0300

    feat(domain): :sparkles: implementa o servico responsavel pela manipulacao de tasks (criacao, delecao, ...)

---
+++ nivel/6/tests/project/internal/services/tasks_services.go
@@ -0,0 +1,60 @@
+package services
+
+import "rocketseat-go/internal/store"
+
+type TaskService struct {
+	Store store.TaskStore
+}
+
+func NewTaskService(store store.TaskStore) *TaskService {
+	return &TaskService{Store: store}
+}
+
+func (s *TaskService) CreateTask(
+	title,
+	description string,
+	priority int32,
+) (store.Task, error) {
+	task, err := s.Store.CreateTask(title, description, priority)
+	if err != nil {
+		return store.Task{}, err
+	}
+	return task, nil
+}
+
+func (s *TaskService) GetTaskByID(id int32) (store.Task, error) {
+	task, err := s.Store.GetTaskByID(id)
+	if err != nil {
+		return store.Task{}, err
+	}
+	return task, nil
+}
+
+func (s *TaskService) ListTasks() ([]store.Task, error) {
+	tasks, err := s.Store.ListTasks()
+	if err != nil {
+		return nil, err
+	}
+	return tasks, nil
+}
+
+func (s *TaskService) UpdateTask(
+	id int32,
+	title,
+	description string,
+	priority int32,
+) (store.Task, error) {
+	task, err := s.Store.UpdateTask(id, title, description, priority)
+	if err != nil {
+		return store.Task{}, err
+	}
+	return task, nil
+}
+
+func (s *TaskService) DeleteTask(id int32) error {
+	err := s.Store.DeleteTask(id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
