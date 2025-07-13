dfdf47f38dd87951d6c28d162faadb50d28dc8bc
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2025-07-13 15:37:55 -0300

    feat(infra): :sparkles: implementa a funcionalidade de manipulacao do banco (migrate).

--- nivel/6/tests/project/cmd/terndotenv/main.go
+++ nivel/6/tests/project/cmd/terndotenv/main.go
@@ -1,1 +1,32 @@
-package terndotenv
+package main
+
+import (
+	"fmt"
+	"os/exec"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	cmd := exec.Command(
+		"tern",
+		"migrate",
+		"--migrations",
+		"internal/store/pgstore/migrations",
+		"--config",
+		"internal/store/pgstore/migrations/tern.conf",
+	)
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("Error executing command:", err)
+		fmt.Println(string(output))
+		return
+	}
+
+	fmt.Println("Executed with success", string(output))
+}
