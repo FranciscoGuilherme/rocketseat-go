fb66e464e673bacb758a4a2e52ca9fbfac77447d
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2025-07-13 21:01:37 -0300

    feat(infra): :sparkles: adiciona a definicao da service no tipo Application.

--- nivel/6/tests/project/internal/api/api.go
+++ nivel/6/tests/project/internal/api/api.go
@@ -1,9 +1,12 @@
 package api
 
 import (
+	"rocketseat-go/internal/services"
+
 	"github.com/go-chi/chi/v5"
 )
 
 type Application struct {
-	Router *chi.Mux
+	Router      *chi.Mux
+	TasksService services.TaskService
 }
